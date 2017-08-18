0b609034df0e5b6a111708be95f694c1ceb69123
Austin DeGarmo <austindegarmo@yahoo.com>
2017-08-17 22:57:49 -0700

    first push

---
+++ server.js
@@ -0,0 +1,73 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var Article = require("./models/Article");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/group-nyt-react");
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("Mongoose Error: ", err);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/api/saved", function(req, res) {
+	Article.find({}).sort([
+		["date", "descending"]
+		]).limit(5).exec(function(err,doc) {
+			if (err) {
+				console.log(err);
+			}
+			else {
+				res.send(doc);
+			}
+		});
+	
+
+});
+
+app.post("/api/saved", function(req, res) {
+	console.log("BODY: " + req.body.title);
+
+	Article.create({
+		title: req.body.title,
+		date: Date.now()
+	}, function(err) {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			res.send("Saved Article")
+		}
+	})
+
+});
+
+app.delete("/api/saved", function(req, res) {
+
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
